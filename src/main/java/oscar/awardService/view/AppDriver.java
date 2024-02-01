eb48056478a4b6167bcf92883342dee0f9670e3d
stefone70 <info@stephanedreux.com>
2024-02-01 16:04:23 -0500

    test de stef sur view de award.services

--- src/main/java/oscar/awardService/view/AppDriver.java
+++ src/main/java/oscar/awardService/view/AppDriver.java
@@ -6,6 +6,8 @@ package oscar.awardService.view;
 
 public class AppDriver {
     public static void main(String[] args) {
+
         System.out.println(" Projet Oscar in progress");
+        System.out.println("Test du Mongolien");
     }
 }
