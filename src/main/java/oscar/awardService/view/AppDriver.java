3179224cc727908d55a670a4c1a2cf391b8a6f83
HelixQc <156359638+HelixQc@users.noreply.github.com>
2024-02-07 13:34:47 -0500

    /***add sql-data.txt file and SQL-shema.txt ***/ /*** test comment have been add to awardService.view.AppDriver.java

--- src/main/java/oscar/awardService/view/AppDriver.java
+++ src/main/java/oscar/awardService/view/AppDriver.java
@@ -6,6 +6,11 @@ package oscar.awardService.view;
 
 public class AppDriver {
     public static void main(String[] args) {
+
         System.out.println(" Projet Oscar in progress");
+
+
+
+        System.out.println("Look fine to so far...");
     }
 }
--- src/main/java/oscar/electionServices/view/AppDriver.java
+++ src/main/java/oscar/electionServices/view/AppDriver.java
@@ -7,6 +7,12 @@ package oscar.electionServices.view;
 
 public class AppDriver {
     public static void main(String[] args) {
-        System.out.println(" Projet Oscar in progress");
+        System.out.println("Projet Oscar in progress");
+
+
+
+
+
+        System.out.println("Look fine to so far...");
     }
 }
