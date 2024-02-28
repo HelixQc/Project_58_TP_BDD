22e574c0b0ade875d3b9c6aa280451c98df99acf
Christophe <arlasam14@gmail.com>
2024-02-28 15:03:49 -0500

    Documentation added

--- src/main/java/oscar/electionServices/persistence/ElectorDAO.java
+++ src/main/java/oscar/electionServices/persistence/ElectorDAO.java
@@ -5,30 +5,56 @@ import oscar.electionServices.model.Elector;
 
 import java.util.List;
 
+/**
+ * The ElectorDAO class provides methods to perform CRUD operations on electors.
+ * It interacts with the ElectorRepository to access elector data.
+ */
 public class ElectorDAO implements IElectorDAO {
 
     ElectorRepository repo = ElectorRepository.getInstance();
 
+    /**
+     * Creates a new elector.
+     * @param e The Elector object to create
+     */
     @Override
     public void createElector(Elector e) {
 
     }
 
+    /**
+     * Deletes an elector.
+     * @param e The Elector object to delete
+     */
     @Override
     public void deleteElector(Elector e) {
         repo.getAllElectors().remove(e);
     }
 
+    /**
+     * Retrieves all electors.
+     * @return A list containing all electors
+     */
     @Override
     public List<Elector> readElector() {
         return repo.getAllElectors();
     }
 
+    /**
+     * Updates an elector.
+     * @param e The Elector object to update
+     * @return The updated Elector object
+     */
     @Override
     public Elector updateElector(Elector e) {
         return null;
     }
 
+    /**
+     * Finds an elector by its weight.
+     * @param weight The weight of the elector to find
+     * @return The Elector object if found, null otherwise
+     */
     public Elector findElectorByWeight(int weight){
         for(int i = 0; i < repo.getAllElectors().size(); i++){
             if(repo.getAllElectors().get(i).getWeight() == weight){
