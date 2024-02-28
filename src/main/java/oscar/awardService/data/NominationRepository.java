22987e311ea34012df76fafec9e2f8b77e13b2be
Christophe <arlasam14@gmail.com>
2024-02-28 14:18:10 -0500

    Documentation added

--- src/main/java/oscar/awardService/data/NominationRepository.java
+++ src/main/java/oscar/awardService/data/NominationRepository.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *Documentation
+ * The NominationRepository class represents a repository for managing nominations.
+ * It provides methods to interact with the nominations data.
  */
     public class NominationRepository {
 
@@ -16,13 +17,20 @@ import java.util.List;
         // List to hold nominations
         private List<Nomination> nominations;
 
-        // Private constructor to prevent instantiation from outside
+        /**
+         * Private constructor to prevent instantiation from outside the class.
+         * Initializes the nominations list and populates it with initial data.
+         */
         private NominationRepository() {
             nominations = new ArrayList<>();
             populateNomination();
         }
 
-        // Method to get the singleton instance
+        /**
+         * Returns the singleton instance of NominationRepository.
+         * If the instance is not yet created, it creates one.
+         * @return The singleton instance of NominationRepository
+         */
         public static NominationRepository getInstance() {
             if (instance == null) {
                 instance = new NominationRepository();
@@ -30,6 +38,10 @@ import java.util.List;
             return instance;
         }
 
+        /**
+         * Populates the nominations list with initial data.
+         * This method is called once during the initialization of the repository.
+         */
         private void populateNomination(){
             Nomination nomination = new Nomination(1,2024, 8.5, "Harry Potter", new ArrayList<>(), AwardRepository.getInstance().getAllAwards());
             nominations.add(nomination);
